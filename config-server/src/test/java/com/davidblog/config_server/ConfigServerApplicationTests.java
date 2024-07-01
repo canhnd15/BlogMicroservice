2aaa91290fb8bf0cfec36c7cf7a28b89a1ba6d05
canhnd <canhnd15@gmail.com>
2024-07-01 16:53:56 +0700

    init project

---
+++ config-server/src/test/java/com/davidblog/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidblog.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/davidblog/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidblog.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
